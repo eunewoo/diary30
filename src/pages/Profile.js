6d624003cb87b55123d6b08ae207ccd36446a4f7
Sungwoong Ritchie Park <ritchiepark6@gmail.com>
2022-11-20 20:59:17 +0900

    setState for each value on Profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,34 +1,58 @@
+import { useState } from "react";
 import Topnav from './nav';
 
-function profile() {
+export default function Profile(props) {
+    const [img, setImg] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [address1, setAddress1] = useState("");
+    const [address2, setAddress2] = useState("");
+
+    const setProfilePicture =(e) => {
+    }
+    const setUsername =(e) => {
+        setName(e.target.value);
+    }
+    const setEmailAddress =(e) => {
+        setEmail(e.target.value);
+    }
+    const setAddress1f =(e) => {
+        setAddress1(e.target.value);
+    }
+    const setAddress2f =(e) => {
+        setAddress2(e.target.value);
+    }
+    const setProfile =(e) => {
+        console.log(img,name,email,address1,address2);
+    }
+
     return(
         <>
             <Topnav />
-            
             <form>
                 <p>Edit Profile</p>
                 <div>
                     <p>Profile Photo</p>
                     <img></img>
-                    <button>Choose new image</button>
-                    <button>Remove image</button>
+                    <button type='button'>Choose new image</button>
+                    <button type='button'>Remove image</button>
                 </div>
                 <div>
-                    <label>Name</label>
-                    <input></input>
+                    <label htmlFor='name'>Name</label>
+                    <input type='text' id='name' onChange={setUsername}></input>
                 </div>
                 <div>
-                    <label>Email</label>
-                    <input></input>
+                    <label htmlFor='email' >Email</label>
+                    <input type='email' id='email' onChange={setEmailAddress}></input>
                 </div>
                 <div>
-                    <label></label>
-                    <input></input>
-                    <input></input>
+                    <label>Address</label>
+                    <input type="text" id='address1' onChange={setAddress1f}></input>
+                    <input type="text" id='address2' onChange={setAddress2f}></input>
                 </div>
-                <button>Save</button>
-                <button>Logout</button>
+                <button type='button' onClick={setProfile}>Save</button>
+                <button type='button'>Logout</button>
             </form>
         </>
     );
-} export default profile;
+}
