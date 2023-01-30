75b3efe603b781227826a1eddd2cdafaf11b93da
Rhqtn <rjsdnsla159@naver.com>
2023-01-30 19:36:23 +0900

    update a alert when user refresh
    alert asking whether you would really leave the page.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Edit from './pages/Edit.js';
-import Log from './pages/Log.js';
-import Login from './pages/Login.js';
-import Register from './pages/Register.js';
-import View from './pages/View.js';
-import Profile from './pages/Profile.js';
-import React, {useState} from 'react';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Edit from "./pages/Edit.js";
+import Log from "./pages/Log.js";
+import Login from "./pages/Login.js";
+import Register from "./pages/Register.js";
+import View from "./pages/View.js";
+import Profile from "./pages/Profile.js";
+import React, { useState, useEffect } from "react";
 
 import { RecoilRoot } from "recoil";
 
-
 function App() {
-  const [id,setId] = useState({});
-  const [profile, setProfile]= useState({
+  const [id, setId] = useState({});
+  const [profile, setProfile] = useState({
     user_id: "",
     password: "",
-    profile: '',
+    profile: "",
     name: "",
-    email: '',
-    address1 : '',
-    address2 : ''
+    email: "",
+    address1: "",
+    address2: "",
   });
   const [questions, setQuestions] = useState({});
 
@@ -28,16 +27,41 @@ function App() {
     setProfile(pf);
   }
 
+  useEffect(() => {
+    window.addEventListener("beforeunload", (e) => {
+      e.preventDefault();
+      e.returnValue = "";
+    });
+
+    return () => {
+      window.removeEventListener("beforeunload", (e) => {
+        e.preventDefault();
+        e.returnValue = "";
+      });
+    };
+  }, []);
+
   return (
     <RecoilRoot>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Login ChangeProfile={ChangeProfile}/>} />
+          <Route
+            exact
+            path="/"
+            element={<Login ChangeProfile={ChangeProfile} />}
+          />
           <Route exact path="/register" element={<Register />} />
-          <Route exact path="/log"  element={<Log profile={profile}/>} />
-          <Route exact path="/edit" element={<Edit profile={profile}/>} />
-          <Route exact path="/view" element={<View profile={profile}/>} />
-          <Route exact path="/profile" element={<Profile profile={profile} changeProfile={ChangeProfile}/>} />
+          <Route exact path="/log" element={<Log profile={profile} />} />
+          <Route exact path="/edit" element={<Edit profile={profile} />} />
+          <Route exact path="/view" element={<View profile={profile} />} />
+          <Route
+            exact
+            path="/profile"
+            element={
+              <Profile profile={profile} changeProfile={ChangeProfile} />
+            }
+          />
+          ;
         </Routes>
       </BrowserRouter>
     </RecoilRoot>
