1d67636a81bf9131e7e95756d5ea049c1dedbe92
Rhqtn <rjsdnsla159@naver.com>
2023-02-15 21:06:01 +0900

    Merge branch 'fixSubmitErrorEun2' into fixS3toCloudinary

--- src/model/states.js
+++ src/model/states.js
@@ -4,3 +4,47 @@ export const DisplayImageAtom = atom({
   key: "displayImage",
   default: "",
 });
+
+// make cloudinary form for Register.js and Profile.js
+export function makeFormData(e) {
+  const img = e.target.files[0];
+
+  const formData = new FormData();
+  formData.append("file", img);
+  formData.append("api_key", "672365852293431");
+  formData.append("upload_preset", "tdc1f5a8");
+  formData.append("timestamp", (Date.now() / 1000) | 0);
+  console.log(formData);
+
+  const config = {
+    headers: { "Content-Type": "multipart/form-data" },
+  };
+  return { formData, config };
+}
+export const returneeState = atom({
+  key: "returneeState",
+  default: [],
+});
+
+export const questionsState = atom({
+  key: "questionsState",
+  default: [],
+});
+
+export const questionsSelector = selector({
+  key: "questionsSelector",
+  get: ({ get }) => get(questionsState),
+  set: ({ set }, newValue) => {
+    set(questionsState, newValue);
+  },
+});
+
+const cum_date = new Date();
+export const cumDateState = atom({
+  key: "cumDateState",
+  default: {
+    cum_year: cum_date.getFullYear(),
+    cum_month: cum_date.getMonth() + 1,
+    cum_day: cum_date.getDate(),
+  },
+});
